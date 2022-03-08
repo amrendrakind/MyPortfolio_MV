3e98ecf87ac07ea3ce2bcf4149ada239f23731ce
amrendrakind <vision2010@gmail.com>
2022-03-08 12:18:17 +0530

    Toggle Mobile menu updated

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,4 @@
 const humberger = document.querySelector('#menu-icon');
-
-const listItem = document.querySelectorAll('a');
-
-for (let i = 1; i < listItem.length; i += 1) {
-  listItem[i].style.display = 'flex';
-  listItem[i].style.alignItems = 'center';
-  listItem[i].style.fontSize = '32px';
-  listItem[i].style.lineHeight = '44px';
-  listItem[i].style.fontWeight = '600';
-  listItem[i].style.color = '#fff';
-  listItem[i].style.paddingBottom = '25px';
-  listItem[i].style.paddingTop = '10px';
-  listItem[i].style.paddingLeft = '20px';
-}
-
 humberger.addEventListener('click', () => {
   const navLinks = document.getElementById('menu-item');
   document.getElementById('logo-icon').style.display = 'none';
@@ -21,6 +6,14 @@ humberger.addEventListener('click', () => {
   document.getElementById('menu-item').style.display = 'flex';
 
   navLinks.classList.add('menu-list-item');
+  const listItem = document.querySelectorAll('a');
+
+  for (let i = 1; i < listItem.length; i += 1) {
+    listItem[i].style.display = 'flex';
+    listItem[i].style.paddingBottom = '25px';
+    listItem[i].style.paddingTop = '20px';
+    listItem[i].style.paddingLeft = '20px';
+  }
 });
 
 const cancel = document.querySelector('.cancel');
@@ -30,11 +23,13 @@ cancel.addEventListener('click', () => {
   document.getElementById('menu-icon').style.display = 'flex';
 });
 // const listItem = document.querySelectorAll('.list-items');
+const listItem = document.querySelectorAll('a');
 
 for (let i = 0; i < listItem.length; i += 1) {
   listItem[i].addEventListener('click', () => {
     document.getElementById('menu-item').style.display = 'none';
     document.getElementById('logo-icon').style.display = 'flex';
     document.getElementById('menu-icon').style.display = 'flex';
+    window.location.reload();
   });
 }
